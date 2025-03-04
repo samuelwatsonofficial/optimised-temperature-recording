bcb8d7b93d7307d44485098b524241c9b83e1169
SamuelWatsonOfficial <84926729+samuelwatsonofficial@users.noreply.github.com>
2025-03-04 09:12:18 +0000

    First upload

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+#include <stdio.h>
+#define PI 3.14159265358979323846
+void setup() {
+    Serial.begin(9600);
+
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+    Serial.print("helo ;3");
+}
